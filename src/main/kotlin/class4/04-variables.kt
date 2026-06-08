83b75a05dd21df348b9fd3d2e633a3d49be052f1
adan_due <ing.adanduenas@gmail.com>
2026-06-08 12:45:30 -0600

    variables

---
+++ src/main/kotlin/class4/04-variables.kt
@@ -0,0 +1,32 @@
+package class4
+
+fun main() {
+    val userName = "Adan"
+    var userName2 = "Carlos"
+
+    userName2 = "PABLO"
+
+    println(userName2)
+
+    var emailUnread = 5
+    emailUnread = 3
+    println("Usiario: $userName, Emails: $emailUnread")
+
+    val totalEmail:Int = 150
+    val totalEmail2:Long = 1500000
+
+    val prcentaje: Float = 75.5f
+    val porcentaj2: Double = 150.2
+
+    val asunto: String = "Reunión"
+
+    val esUrgente: Boolean = true
+
+    val numero = 42
+
+    val comoTexto = numero.toString()
+    val textoNumero = "25".toInt()
+
+    println(numero)
+    println(textoNumero)
+}
